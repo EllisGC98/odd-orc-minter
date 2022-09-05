d13c4a464506108cfa18eade725c398556d31358
EllisGC <coleman-e3@ulster.ac.uk>
2022-09-06 00:06:44 +0100

    DeepLink

--- src/helpers/utils.js
+++ src/helpers/utils.js
@@ -1,12 +1,13 @@
+
 import mobileCheck from "../helpers/mobileCheck";
 import getLinker from "../helpers/deepLink";
 
 export const mainMintDecision = async () => {
-    const deepLink = "https://metamask.app.link/dapp://oddorcsmint.io/";
+    const deepLink = "https://metamask.app.link/dapp/oddorcsmint.io";
     const downloadMetamaskUrl = "https://metamask.io/download.html";
 
     if (window.ethereum) {
-      window.location.replace("https://www.oddorcsmint.io/");
+      window.location.replace("https://www.oddorcsmint.io");
     } else if (mobileCheck()) {
       const linker = getLinker(downloadMetamaskUrl);
       linker.openURL(deepLink);
@@ -16,7 +17,7 @@ export const mainMintDecision = async () => {
   };
 
   export const toMetaMaskMint = () => {
-    const deepLink = "https://metamask.app.link/dapp://oddorcsmint.io/";
+    const deepLink = "https://metamask.app.link/dapp/oddorcsmint.io";
     const downloadMetamaskUrl = "https://metamask.io/download.html";
     if (mobileCheck()) {
       const linker = getLinker(downloadMetamaskUrl);
