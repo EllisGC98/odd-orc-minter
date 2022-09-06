68a96b10e0bf67f629260d299873c04a79fcdd22
EllisGC <coleman-e3@ulster.ac.uk>
2022-09-07 00:49:21 +0100

    revert back to prev connectAccounts

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,7 @@ import { Box, Button, Flex, Image, Link, Text } from '@chakra-ui/react';
 import Twitter from "./assets/Twitter.png";
 import Opensea from "./assets/Opensea.png";
 import Discord from "./assets/Discord.png";
-import { toMetaMaskMint } from "./helpers/utils";
+
 
 
 /*const shortenAddress = (address) => {
@@ -12,7 +12,6 @@ import { toMetaMaskMint } from "./helpers/utils";
       address.length
     )}`;
   } */
-
 
 const NavBar = ({ accounts, setAccounts }) => {
     const isConnected = Boolean(accounts[0]);
@@ -24,10 +23,6 @@ const NavBar = ({ accounts, setAccounts }) => {
                 method: "eth_requestAccounts",
             });
             setAccounts(accounts);
-        
-    } else {
-       toMetaMaskMint();
-
     }
 }
 
