656fe69384907d6e5dc863cd2a318ac59113969a
EllisGC <coleman-e3@ulster.ac.uk>
2022-09-07 09:26:10 +0100

    MainMint is breaking website in mobile

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import './App.css';
+import MainMint from './MainMint';
 import NavBar from './NavBar';
 
 function App() {
@@ -10,7 +11,7 @@ function App() {
     <div  className="overlay">
       <div className="App">
         {<NavBar accounts={accounts} setAccounts={setAccounts} />}
-        {/*<MainMint accounts={accounts} setAccounts={setAccounts} />*/}
+        {<MainMint accounts={accounts} setAccounts={setAccounts} />}
       </div>
       <div className="background"></div> 
     </div> 
