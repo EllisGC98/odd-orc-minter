d729f549d1bcf156480d71ab18fff46e54d040d2
EllisGC <coleman-e3@ulster.ac.uk>
2023-05-13 21:44:05 +0100

    Removing socials

--- src/NavBar.js
+++ src/NavBar.js
@@ -1,8 +1,5 @@
 import React from "react";
-import { Box, Button, Flex, Image, Link, Text } from '@chakra-ui/react';
-import Twitter from "./assets/Twitter_icon.png";
-import Opensea from "./assets/Opensea_Icon.png";
-import Discord from "./assets/Discord_Icon.png";
+import { Box, Button, Flex, Text } from '@chakra-ui/react';
 import { toMetaMaskMint } from "./helpers/utils";
 
 
@@ -32,52 +29,7 @@ const NavBar = ({ accounts, setAccounts }) => {
     return (
         <Flex justify="space-between" align="center" padding="30px">
           {/* Left Side - Social Media */}
-          <Flex className="social-buttons" justify="space-between" width="10%" padding="0 40px"
-          css={{ 
-            "@media screen (min-width: 450px) and (max-width: 767px)": {
-             
-            },
-          }} >
-            <Link
-              href="https://twitter.com/OddOrcs"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image src={Twitter} boxSize="35px" margin="0 25px" transition="transform 0.3s ease-in-out"
-              _hover={{transform: "scale(1.3)",}} 
-              css={{ 
-                "@media screen and (max-width: 767px)": {
-              
-                },
-              }}/>
-            </Link>
-            <Link
-              href="https://opensea.io/collection/odd-orcs"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image src={Opensea} boxSize="35px" margin="0 25px" transition="transform 0.3s ease-in-out"
-              _hover={{transform: "scale(1.3)",}}  
-              css={{ 
-                "@media screen and (max-width: 767px)": {
-                 
-                },
-              }}/>
-            </Link>
-            <Link
-              href="http://discord.gg/OddOrcs"
-              target="_blank"
-              rel="noopener noreferrer"
-            >
-              <Image src={Discord} boxSize="35px" margin="0 25px" transition="transform 0.3s ease-in-out"
-              _hover={{transform: "scale(1.3)",}}
-              css={{ 
-                "@media screen and (max-width: 767px)": {
-               
-                },
-              }}/>
-            </Link>
-          </Flex>
+         
   
           {isConnected ? (
             <Box margin="0 15px" fontSize="20px">
