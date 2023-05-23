a232380e57000ea8d6f74e7fa59dc543b1a3db58
EllisGC <coleman-e3@ulster.ac.uk>
2023-05-23 18:54:27 +0100

    Updating Mint Details

--- src/MainMint.js
+++ src/MainMint.js
@@ -179,7 +179,7 @@ const MainMint = ({ accounts, setAccounts }) => {
                 </div>
               </div>
               <Text fontSize="25px">
-                MINT IS PAUSED{" "}
+                MINTING SOON{" "}
               </Text>
               {isConnected ? (
                 <div>
